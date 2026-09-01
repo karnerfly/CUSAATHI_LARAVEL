fc77c90542fcada0fe3fa6ee1980628c2d62d874
Toufique Al Ajay <87595397+karnerfly@users.noreply.github.com>
2026-09-01 10:30:37 +0530

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -10,3 +10,9 @@ php artisan route:cache
 
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Running database seeders..."
+
+php artisan db:seed --force
+
+echo "Laravel deployment completed."
