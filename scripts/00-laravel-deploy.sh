1a6afafb7a3cd584d56dbde249f772cd68115804
Toufique Al Ajay <87595397+karnerfly@users.noreply.github.com>
2026-09-01 12:11:06 +0530

    Update 00-laravel-deploy.sh

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,6 +2,9 @@
 echo "Running composer"
 composer install --no-dev --working-dir=/var/www/html
 
+echo "Clearing old cache"
+php artisan optimize:clear
+
 echo "Caching config..."
 php artisan config:cache
 
