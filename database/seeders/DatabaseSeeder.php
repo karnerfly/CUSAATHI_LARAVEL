97446703ed514bea9721e411b039e8c09aa0020b
Toufique Al Ajay <87595397+karnerfly@users.noreply.github.com>
2026-09-08 17:40:28 +0530

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Admin;
-use App\Models\Permission;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,51 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Permission::factory()->createMany([
-            [
-                'name' => 'create permission',
-                'ability' => 'create:permission',
-                'category' => 'permissions',
-            ],
-            [
-                'name' => 'read permission',
-                'ability' => 'read:permission',
-                'category' => 'permissions',
-            ],
-            [
-                'name' => 'update permission',
-                'ability' => 'update:permission',
-                'category' => 'permissions',
-            ],
-            [
-                'name' => 'delete permission',
-                'ability' => 'delete:permission',
-                'category' => 'permissions',
-            ],
-            [
-                'name' => 'assign permission',
-                'ability' => 'assign:permission',
-                'category' => 'permissions',
-            ],
-            [
-                'name' => 'revoke permission',
-                'ability' => 'revoke:permission',
-                'category' => 'permissions',
-            ],
-        ]);
-
-        $admin = Admin::factory()->create([
-            'name' => 'Toufique Al Ajay',
-            'email' => 'toufique26ajay@gmail.com',
-        ]);
-
-        $permissions = Permission::all();
-
-        foreach ($permissions as $permission) {
-            DB::table('admin_permission')->insert([
-                'admin_id' => $admin->id,
-                'permission_id' => $permission->id,
-            ]);
-        }
+        //
     }
 }
